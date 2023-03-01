a526d238b725f5c71f804dc308ce4b033acedf4d
Pedro Gomes <112027917+Gomezxk@users.noreply.github.com>
2023-03-01 18:16:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+ echo "Olá Mundo!! </br>";
+
+ echo "meu primeiro código em php </br>";
+
+echo "3 + 3 = "; 
+echo 3 + 3; 
+echo "</br> 18 * 190 = ";
+echo 18 * 190;
+echo "</br> 45 / 20 = ";
+echo 45 / 20;
+
+$nome = "Pedro";
+$sobre = "Gomez";
+
+echo "Olá Mundo!! </br>";
+
+ echo "$nome $sobre primeiro código em php </br>";
+ ?>
